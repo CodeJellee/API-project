82617f1d0260e539dd2cfe5e6f665e582ac40008
Jennifer Lee <leejpac@gmail.com>
2023-06-12 21:52:36 -0400

    add demo reviews

--- backend/db/seeders/20230612233043-demo-review.js
+++ backend/db/seeders/20230612233043-demo-review.js
@@ -1,6 +1,13 @@
 'use strict';
+const bcrypt = require("bcryptjs");
 
 /** @type {import('sequelize-cli').Migration} */
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
 module.exports = {
   async up (queryInterface, Sequelize) {
     /**
@@ -12,6 +19,27 @@ module.exports = {
      *   isBetaMember: false
      * }], {});
     */
+    options.tableName = 'Reviews';
+    return queryInterface.bulkInsert(options, [
+      {
+        spotId: '1',
+        userId: '1',
+        review: 'Beautiful Springfield but meh.',
+        stars: '2'
+      },
+      {
+        spotId: '2',
+        userId: '2',
+        review: 'Stunning Boulder',
+        stars: '5'
+      },
+      {
+        spotId: '3',
+        userId: '3',
+        review: 'Such a rainy place here.',
+        stars: '3'
+      }
+    ], {});
   },
 
   async down (queryInterface, Sequelize) {
@@ -21,5 +49,10 @@ module.exports = {
      * Example:
      * await queryInterface.bulkDelete('People', null, {});
      */
+    options.tableName = 'Reviews';
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete(options, {
+      spotId: { [Op.in]: ['1', '2', '3'] }
+    }, {});
   }
 };
