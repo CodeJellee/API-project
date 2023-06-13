7e93070f8074b3e4bad3d781f596d6d93f77fff9
Jennifer Lee <leejpac@gmail.com>
2023-06-12 21:59:03 -0400

    add demo ReviewImages

--- backend/db/seeders/20230612233058-demo-review-image.js
+++ backend/db/seeders/20230612233058-demo-review-image.js
@@ -1,6 +1,13 @@
 'use strict';
+const bcrypt = require("bcryptjs");
 
 /** @type {import('sequelize-cli').Migration} */
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
 module.exports = {
   async up (queryInterface, Sequelize) {
     /**
@@ -12,6 +19,22 @@ module.exports = {
      *   isBetaMember: false
      * }], {});
     */
+
+    options.tableName = 'ReviewImages';
+    return queryInterface.bulkInsert(options, [
+      {
+        reviewId: '1',
+        url: 'https://upload.wikimedia.org/wikipedia/commons/d/de/6T4A9446-HDR-3.jpg'
+      },
+      {
+        reviewId: '2',
+        url: 'https://upload.wikimedia.org/wikipedia/commons/8/80/Flatirons_Winter_Sunrise_edit_2.jpg'
+      },
+      {
+        reviewId: '3',
+        url: 'https://upload.wikimedia.org/wikipedia/commons/5/58/Seattle_Center_as_night_falls.jpg'
+      }
+    ], {});
   },
 
   async down (queryInterface, Sequelize) {
@@ -21,5 +44,10 @@ module.exports = {
      * Example:
      * await queryInterface.bulkDelete('People', null, {});
      */
+    options.tableName = 'ReviewImages';
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete(options, {
+      reviewId: { [Op.in]: ['1', '2', '3'] }
+    }, {});
   }
 };
