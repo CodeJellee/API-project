f54e64285b5d0dde707534bbc28ca843ddc553c5
Jennifer Lee <leejpac@gmail.com>
2023-06-13 19:19:46 -0400

    fix assocation m2m

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -4,7 +4,7 @@ const { Model, Validator } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     /**
-     * Helper method for defining associations.
+     * Helper method for defining associations.gi
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
