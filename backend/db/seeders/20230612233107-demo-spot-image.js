88ed25c618d9fec1ea4a91cb747d7be22b4e7879
Jennifer Lee <leejpac@gmail.com>
2023-06-12 22:09:38 -0400

    add demo SpotImages

--- backend/db/seeders/20230612233107-demo-spot-image.js
+++ backend/db/seeders/20230612233107-demo-spot-image.js
@@ -1,6 +1,13 @@
 'use strict';
+const bcrypt = require("bcryptjs");
 
 /** @type {import('sequelize-cli').Migration} */
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
 module.exports = {
   async up (queryInterface, Sequelize) {
     /**
@@ -12,6 +19,25 @@ module.exports = {
      *   isBetaMember: false
      * }], {});
     */
+
+    options.tableName = 'SpotImages';
+    return queryInterface.bulkInsert(options, [
+      {
+        spotId: '1',
+        url: 'https://upload.wikimedia.org/wikipedia/commons/d/de/6T4A9446-HDR-3.jpg',
+        preview: true
+      },
+      {
+        spotId: '2',
+        url: 'https://upload.wikimedia.org/wikipedia/commons/8/80/Flatirons_Winter_Sunrise_edit_2.jpg',
+        preview: true
+      },
+      {
+        spotId: '3',
+        url: 'https://upload.wikimedia.org/wikipedia/commons/5/58/Seattle_Center_as_night_falls.jpg',
+        preview: false
+      }
+    ], {});
   },
 
   async down (queryInterface, Sequelize) {
@@ -21,5 +47,11 @@ module.exports = {
      * Example:
      * await queryInterface.bulkDelete('People', null, {});
      */
+
+    options.tableName = 'SpotImages';
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete(options, {
+      spotId: { [Op.in]: ['1', '2', '3'] }
+    }, {});
   }
 };
