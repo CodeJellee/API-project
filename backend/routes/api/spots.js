7562cee9588add3f7f8f273b4481497cf886692f
Jennifer Lee <leejpac@gmail.com>
2023-06-13 22:47:14 -0400

    add /

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -12,6 +12,32 @@ const { handleValidationErrors } = require('../../utils/validation');
 
 const router = express.Router();
 
+//GET ALL SPOTS
+router.get('/', async(req, res, next) => {
+    const allSpots = await Spot.findAll({
+        attributes: {
+            include: [
+            [sequelize.fn('AVG', sequelize.col("Reviews.stars")), "avgRating"], //Sequelize function that generates a function call in SQL-AVG and generates a column reference, alias w/ second param
+            [sequelize.literal('(SELECT url FROM SpotImages WHERE SpotImages.spotId = Spot.id)'), 'previewImage'], //provide a raw SQL expression -> selecting url from SpotImages where they match, w/ alias to second param
+            ]
+        },
+
+        include:[
+        {
+            model: Review,
+            attributes: []
+        },
+        {
+            model: SpotImage,
+            attributes: []
+        }
+        ],
+
+        group: ['Spot.id'] //group query result based on 'id' column of the 'Spot' model- needed this bc only 1 spot was printing
+    })
+    return res.json({Spots: allSpots})
+})
+
 //GET ALL SPOTS OWNED BY THE CURRENT USER
 router.get('/current', requireAuth, async (req, res, next) => {
     const currentUserSpots = await Spot.findAll({
@@ -71,6 +97,15 @@ router.get('/current', requireAuth, async (req, res, next) => {
 //     total += review.stars
 // })
 // let avgRating = count.length > 0 ? total / count.length : null
+
+
+// if (!currentUserSpots) {
+//     res.status(404)
+//     return res.json({
+//         message: "Spot couldn't be found"
+//     })
+// }
+
 // })
 
 module.exports = router;
