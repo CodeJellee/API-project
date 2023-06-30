38a7ece7c6282d78698fb2b86b252190ac777762
Jennifer Lee <leejpac@gmail.com>
2023-06-30 01:34:20 -0400

    login form page done

---
+++ frontend/src/store/session.js
@@ -0,0 +1,56 @@
+import {csrfFetch} from './csrf'
+
+/** Action Type Constants*/
+export const SET_USER = 'session/setUser'
+export const REMOVE_USER = 'session/removeUser'
+
+/** Action Creators*/
+export const setUser = (user) => { //return this way
+    return {
+        type: SET_USER,
+        payload: user
+    };
+};
+
+export const noSessionUser = () => ({ //or return this way using ()
+    type: REMOVE_USER
+})
+
+/** Thunk Action Creators */
+
+export const login = (user) => async (dispatch) => {
+    const {credential, password} = user;
+    const response = await csrfFetch('/api/session', {
+        method: "POST",
+        body: JSON.stringify({
+            credential,
+            password,
+        }),
+    });
+    const data = await response.json();
+    dispatch(setUser(data.user));
+    return response;
+}
+
+
+/** Reducer*/
+const initialState = {user: null}
+
+const sessionReducer = (state = initialState, action) => {
+    let newState;
+
+    switch(action.type){
+        case SET_USER:
+            newState = Object.assign({}, state);
+            newState.user = action.payload;
+            return newState;
+        case REMOVE_USER:
+            newState = Object.assign({}, state);
+            newState.user = null;
+            return newState;
+        default:
+            return state
+    }
+}
+
+export default sessionReducer;
