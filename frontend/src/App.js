edb641939e83d5cd5d7fba3ed9f84131b09c0366
Jennifer Lee <leejpac@gmail.com>
2023-06-30 15:44:43 -0400

    phase 3 done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,9 +3,9 @@ import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { Route, Switch } from "react-router-dom";
 import LoginFormPage from "./components/LoginFormPage";
-import SignupFormPage from "./components/SignUpFormPage";
-import Navigation from "./components/Navigation";
+import SignupFormPage from "./components/SignupFormPage";
 import * as sessionActions from "./store/session";
+import Navigation from "./components/Navigation";
 
 function App() {
   const dispatch = useDispatch();
@@ -30,6 +30,5 @@ function App() {
     </>
   );
 }
-
 
 export default App;
