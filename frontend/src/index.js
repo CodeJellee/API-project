e15edaf4581d9311de2cf6c69f302218f8ed4987
Jennifer Lee <leejpac@gmail.com>
2023-06-30 12:35:56 -0400

    phase 2 signup action

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,12 +6,12 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import * as sessionActions from './store/session'
 
 import configureStore from './store';
 
 import { restoreCSRF, csrfFetch } from './store/csrf';
 
+import * as sessionActions from './store/session'
 
 const store = configureStore();
 
@@ -21,6 +21,7 @@ if (process.env.NODE_ENV !== 'production') {
   window.csrfFetch = csrfFetch;
   window.store = store;
   window.sessionActions = sessionActions;
+
 }
 
 
--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -3,9 +3,11 @@ import {csrfFetch} from './csrf'
 /** Action Type Constants*/
 export const SET_USER = 'session/setUser'
 export const REMOVE_USER = 'session/removeUser'
+// export const ADD_USER = 'session/addUser'
 
 /** Action Creators*/
 export const setUser = (user) => { //return this way
+    console.log('SET USER ACTION CREATOR')
     return {
         type: SET_USER,
         payload: user
@@ -15,6 +17,11 @@ export const setUser = (user) => { //return this way
 export const noSessionUser = () => ({ //or return this way using ()
     type: REMOVE_USER
 })
+
+// export const addUser = (user) => ({
+//     type: ADD_USER,
+//     payload: user
+// })
 
 /** Thunk Action Creators */
 
@@ -39,6 +46,26 @@ export const restoreUser = () => async (dispatch) => {
     return response;
   };
 
+  export const signup = (user) => async (dispatch) => {
+    console.log("THUNK: THIS IS SIGNUP")
+    const { username, firstName, lastName, email, password } = user;
+    console.log("DATA IN THUNK")
+    const response = await csrfFetch("/api/users", {
+      method: "POST",
+      body: JSON.stringify({
+        username,
+        firstName,
+        lastName,
+        email,
+        password,
+      }),
+    });
+    console.log('THUNK RESPONSE', response )
+    const data = await response.json();
+    dispatch(setUser(data.user));
+    return response;
+  };
+
 
 /** Reducer*/
 const initialState = {user: null}
@@ -48,6 +75,7 @@ const sessionReducer = (state = initialState, action) => {
 
     switch(action.type){
         case SET_USER:
+            console.log('CASE')
             newState = Object.assign({}, state);
             newState.user = action.payload;
             return newState;
@@ -55,6 +83,11 @@ const sessionReducer = (state = initialState, action) => {
             newState = Object.assign({}, state);
             newState.user = null;
             return newState;
+        // case ADD_USER:
+        //     console.log("CASE HERE")
+        //     newState = Object.assign({}, state);
+        //     newState.user = action.payload;
+        //     return newState;
         default:
             return state
     }
