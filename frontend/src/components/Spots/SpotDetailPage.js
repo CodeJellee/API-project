6a7ad39cddb5ae1e02f55d54dcc838f56bde087c
Jennifer Lee <leejpac@gmail.com>
2023-07-02 03:19:59 -0400

    get spot by id some css

--- frontend/src/components/Spots/SpotDetailPage.js
+++ frontend/src/components/Spots/SpotDetailPage.js
@@ -3,7 +3,6 @@ import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from 'react-redux';
 import { NavLink, useParams } from 'react-router-dom';
 import {fetchGetSpotById} from "../../store/spots";
-import EachSpot from "./GetAllSpots";
 import './Spots.css';
 
 
@@ -25,7 +24,6 @@ const SpotId = () => {
     return (
     <>
         <div id="spot-details-container">
-            <h1>INSERT SPOT DATA HERE</h1>
           <h1>{spots.name}</h1>
           <h2>{spots.city}, {spots.state}, {spots.country}</h2>
           <div>
@@ -33,8 +31,24 @@ const SpotId = () => {
                 <img id={spots.SpotImages[spots.SpotImages.indexOf(spot)].id} src={spots.SpotImages[spots.SpotImages.indexOf(spot)].url}></img>
             ))}
           </div>
-          <div>Hosted by {spots.Owner.firstName} {spots.Owner.lastName} </div>
-          <div>{spots.description}</div>
+          <div>
+            <div>
+                <div>Hosted by {spots.Owner.firstName} {spots.Owner.lastName} </div>
+                <div>{spots.description}</div>
+            </div>
+            <div>
+                <div>$ {spots.price} night, ★ {spots.avgRating} · {spots.numReviews} reviews</div>
+                <button>Reserve</button>
+            </div>
+            <div>
+                <h3>add a line here to seprate above and start of below</h3>
+                <div>★ {spots.avgRating} · {spots.numReviews} reviews</div>
+                <div>
+                    <h3>{spots.Owner.firstName}</h3>
+                    <h4>{spots.createdAt}THIS IS ASS DATE...NEED TO JUST BE MONTH AND YEAR</h4>
+                </div>
+            </div>
+          </div>
         </div>
 
     </>
