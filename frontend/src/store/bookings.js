d4ff3c2c2bcce00b7cddab6a0ce6420f1b3f8a19
Jennifer Lee <leejpac@gmail.com>
2023-08-22 19:59:39 -0400

    delete booking, still needs window console check

--- frontend/src/store/bookings.js
+++ frontend/src/store/bookings.js
@@ -9,6 +9,7 @@ import {csrfFetch} from './csrf'
 //TYPE----------------------------------------------------------------------------------------------//
 const GET_CURRENT_USER_BOOKINGS = 'bookings/GET_CURRENT_USER_BOOKINGS'
 const GET_BOOKINGS_BY_SPOT_ID = 'bookings/GET_BOOKINGS_BY_SPOT_ID'
+const DELETE_BOOKING_BY_BOOKING_ID = 'bookings/DELETE_BOOKING_BY_BOOKING_ID'
 
 
 //ACTION----------------------------------------------------------------------------------------------//
@@ -20,6 +21,11 @@ const actionGetCurrentUserBookings = (bookings) => ({
 const actionGetBookingsBySpotId = (bookings) => ({
     type: GET_BOOKINGS_BY_SPOT_ID,
     bookings
+})
+
+const actionDeleteBookingByBookingId = (bookingId) => ({
+    type: DELETE_BOOKING_BY_BOOKING_ID,
+    bookingId,
 })
 
 //THUNKS----------------------------------------------------------------------------------------------//
@@ -47,13 +53,27 @@ export const thunkGetBookingsBySpotId = (spotId) => async (dispatch) => {
     }
 }
 
+export const thunkDeleteBookingByBookingId = (bookingId) => async (dispatch) => {
+    const res = await csrfFetch(`api/bookings/${bookingId}`, {
+        method: 'DELETE',
+    });
+
+    if (res.ok) {
+        dispatch(actionDeleteBookingByBookingId(bookingId))
+        return true;
+    } else {
+        const errors = await res.json();
+        return errors;
+    }
+}
+
 //REDUCER----------------------------------------------------------------------------------------------//
 const initialState = { currentUserBookings: [] , spotBookings: [], singleBooking: []}
 
 const bookingsReducer = (state = initialState, action) => {
     switch(action.type){
         case GET_CURRENT_USER_BOOKINGS: {
-            let newState = { ...state }
+            const newState = { ...state }
             newState.currentUserBookings = {};
             action.bookings.Bookings.forEach(
                 (booking) => (newState.currentUserBookings[booking.id] = booking)
@@ -61,11 +81,19 @@ const bookingsReducer = (state = initialState, action) => {
             return newState
         }
         case GET_BOOKINGS_BY_SPOT_ID: {
-            let newState = { ...state }
+            const newState = { ...state }
             newState.spotBookings = {};
             action.bookings.Bookings.forEach(
                 (booking) => (newState.spotBookings[booking.id] = booking)
             )
+            return newState
+        }
+        case DELETE_BOOKING_BY_BOOKING_ID: {
+            const newState = { ...state };
+            const bookingId = action.bookingId;
+            delete newState.currentUserBookings[bookingId]
+            delete newState.singleBooking[bookingId]
+            newState.singleBooking = {}
             return newState
         }
 
